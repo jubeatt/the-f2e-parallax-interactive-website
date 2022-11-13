14c5bf7d284cec6bff6e1bc8cd04f92ca6c10c4d
PeaNu <jimdevelopesite@gmail.com>
2022-11-13 15:56:44 +0800

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/the-f2e-parallax-interactive-website/',
   plugins: [react()],
   resolve: {
     alias: {
